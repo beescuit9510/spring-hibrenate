9374140a951c2160a6785bf4624cb255f98bde3d
beescuit9510 <pengsoon20@gmail.com>
2022-03-26 08:27:46 +0900

    fix java.io.FileNotFoundException: class path resource [주소] cannot be opened because it does not exist error

---
+++ spring-demo-one/src/main/java/com/luv2code/springdemo/BeanLifeCycleDemoApp.java
@@ -0,0 +1,28 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new  ClassPathXmlApplicationContext("beanScopeApplicationContext.xml");
+
+        Coach coach = context.getBean("myCoach", Coach.class);
+
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        // check if they are the same
+        boolean result = (coach == alphaCoach);
+
+        System.out.println();
+        System.out.println("Pointing to the same object: "+result);
+        System.out.println();
+        System.out.println("Memory location for coach: "+coach);
+        System.out.println();
+        System.out.println("Memory location for alphaCoach: "+alphaCoach);
+
+
+        context.close();
+    }
+}
