c16dd57d73692c5ea523e53a1a8320686df728d4
beescuit9510 <pengsoon20@gmail.com>
2022-03-26 21:26:01 +0900

    practice spring config @PostConstruct @PreDestroy

--- spring-demo-annotation/src/main/java/com/luv2code/springdemo/UnhappyFortuneService.java
+++ spring-demo-annotation/src/main/java/com/luv2code/springdemo/UnhappyFortuneService.java
@@ -3,6 +3,9 @@ package com.luv2code.springdemo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class UnhappyFortuneService implements FortuneService{
 
@@ -12,5 +15,15 @@ public class UnhappyFortuneService implements FortuneService{
     @Override
     public String getFortune() {
         return fortune;
+    }
+
+    @PostConstruct
+    public void afterConstruct(){
+        System.out.println(">> UnhappyFortuneService: inside afterConstruct() method");
+    }
+
+    @PreDestroy
+    public void beforeDestruction(){
+        System.out.println(">> UnhappyFortuneService: inside beforeDestruction() method");
     }
 }
