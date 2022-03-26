8d9be84403380a902410be03c0e8972bb5314899
beescuit9510 <pengsoon20@gmail.com>
2022-03-26 20:29:44 +0900

    spring config with @Qualifier annotaion

---
+++ spring-demo-annotation/src/main/java/com/luv2code/springdemo/RandomFortuneService.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class RandomFortuneService implements FortuneService{
+
+    private String[] data = {"Fortune1","Fortune2","Fortune3"};
+
+    private Random myRandom = new Random();
+
+    @Override
+    public String getFortune() {
+
+        int max = data.length;
+
+        int index = myRandom.nextInt(max);
+
+        String fortune = data[index];
+
+        return fortune;
+    }
+}
--- spring-demo-annotation/src/main/java/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotation/src/main/java/com/luv2code/springdemo/TennisCoach.java
@@ -1,22 +1,26 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 //@Component("thatSillyCoach") //explicit component names
 @Component //default component names
 public class TennisCoach implements Coach{
+
+    @Autowired
+    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
     public TennisCoach() {
          System.out.println(">> TennisCoach: inside default constructor");
     }
 
-    @Autowired
-    public void doSomeCrazyStuff(FortuneService fortuneService) {
-        System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
-        this.fortuneService = fortuneService;
-    }
+//    @Autowired
+//    public void doSomeCrazyStuff(FortuneService fortuneService) {
+//        System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+//        this.fortuneService = fortuneService;
+//    }
 
     //    @Autowired
 //    public TennisCoach(FortuneService fortuneService) {
