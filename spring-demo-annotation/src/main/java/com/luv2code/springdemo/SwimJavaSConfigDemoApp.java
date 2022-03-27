4c65681aff56cfdd91f4ed71d4b88665acf5447e
beescuit9510 <pengsoon20@gmail.com>
2022-03-27 11:26:04 +0900

    spring Injecting values from properties file

--- spring-demo-annotation/src/main/java/com/luv2code/springdemo/SwimJavaSConfigDemoApp.java
+++ spring-demo-annotation/src/main/java/com/luv2code/springdemo/SwimJavaSConfigDemoApp.java
@@ -3,18 +3,22 @@ package com.luv2code.springdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SwimJavaSConfigDemoApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) {n
 
         //read spring config java class
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 
         //get a bean from context(spring container)
-        Coach coach = context.getBean("swimCoach", Coach.class);
+        SwimCoach coach = context.getBean("swimCoach", SwimCoach.class);
 
         //call methods
         System.out.println(coach.getDailyWorkout());
 
         System.out.println(coach.getDailyFortune());
+
+        System.out.println("email: "+coach.getEmail());
+
+        System.out.println("team: "+coach.getTeam() );
 
         //close the context
         context.close();
