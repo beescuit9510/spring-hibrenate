2d8c3c1636a53831a7d6b3ca91d416f61a80579f
beescuit9510 <pengsoon20@gmail.com>
2022-03-31 08:01:55 +0900

    section 21 :hibernate crud features primary keys

---
+++ hibernate-tutorial/src/main/java/com/luv2code/PrimaryKeyDemo.java
@@ -0,0 +1,47 @@
+package com.luv2code;
+
+import com.luv2code.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class PrimaryKeyDemo {
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+        try {
+            // use the session object to save Java object
+
+            // create 3 student objects
+            System.out.println("creating 3 new student objects");
+            Student tempStudent1 = new Student("pepa","madrigal","pepa@gmail.com");
+            Student tempStudent2 = new Student("bruno","madrigal","bruno@gmail.com");
+            Student tempStudent3 = new Student("dolores","madrigal","dolores@gmail.com");
+
+            // start a transaction
+            session.beginTransaction();
+
+            // save the 3 student objects
+            System.out.println("saving the new student objects");
+            session.save(tempStudent1);
+            session.save(tempStudent2);
+            session.save(tempStudent3);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        }finally {
+            factory.close();
+        }
+    }
+
+
+}
